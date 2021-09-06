d53e5890f2722b4b411c697a5c5c8f610b93196e
ezequiel <ezequielmatiascordoba@gmail.com>
2021-09-05 21:38:35 -0300

    update agregarContacto

--- app/src/main/java/com/example/tp2/AgregarContacto.java
+++ app/src/main/java/com/example/tp2/AgregarContacto.java
@@ -34,26 +34,27 @@ public class AgregarContacto extends AppCompatActivity {
         spinner_email = (Spinner) findViewById(R.id.spinner_tel);
         String [] opciones = {"Casa", "Trabajo", "Móvil"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_tp, opciones);
+        ArrayAdapter<String> adapterEmail = new ArrayAdapter<>(this, R.layout.spinner_item_tp, opciones);
         spinner_tel.setAdapter(adapter);
-        spinner_email.setAdapter(adapter);
+        spinner_email.setAdapter(adapterEmail);
     }
 
     public void MasDatos(View view){
         Intent masDatos = new Intent(this, AgregarContacto_MasDatos.class);
-        masDatos.putExtra("datos",etNombre.getText().toString());
-        masDatos.putExtra("datos",etApellido.getText().toString());
-        masDatos.putExtra("datos",etTelefono.getText().toString());
-        masDatos.putExtra("datos",etMail.getText().toString());
-        masDatos.putExtra("datos",etDireccion.getText().toString());
-        masDatos.putExtra("datos",etFechaNac.getText().toString());
+        masDatos.putExtra("nombre",etNombre.getText().toString());
+        masDatos.putExtra("apellido",etApellido.getText().toString());
+        masDatos.putExtra("telefono",etTelefono.getText().toString());
+        masDatos.putExtra("email",etMail.getText().toString());
+        masDatos.putExtra("direccion",etDireccion.getText().toString());
+        masDatos.putExtra("fecha_nacimiento",etFechaNac.getText().toString());
         
         String tel = spinner_tel.getSelectedItem().toString();
         if(!tel.isEmpty()){
-            masDatos.putExtra("datos",tel);
+            masDatos.putExtra("tipo_telefono",tel);
         }
         String email = spinner_email.getSelectedItem().toString();
         if(!email.isEmpty()){
-            masDatos.putExtra("datos", email);
+            masDatos.putExtra("tipo_email", email);
         }
         
         startActivity(masDatos);
